968439c8cef0c07b2e2cbc0902c87a01a0b4a8d9
zqunagi <qzqaq777@gmail.com>
2024-04-10 17:21:27 +0800

    update md5_generator

--- md5_generator/md5_generator.cc
+++ md5_generator/md5_generator.cc
@@ -1,4 +1,24 @@
+#include "md5.h"
+
 #include <stdio.h>
+
+bool md5_file(const char *file, char checksum[33])
+{
+	MD5 md5;
+	FILE *fp;
+
+	if ((fp = fopen(file, "rb")) == NULL)
+	{
+		ErrorMessage("file %s is NULL", file);
+		return false;
+	}
+
+	md5.update(fp);
+	md5.finalize();
+	md5.hex_digest(checksum);
+
+	return true;
+}
 
 int main(char *argc, int argv)
 {
